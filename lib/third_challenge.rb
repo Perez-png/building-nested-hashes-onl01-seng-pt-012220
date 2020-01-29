c5c9b137d69041facbe9479a852971c321bd4fa6
Xavier Perez <xperez007@gmail.com>
2020-01-29 22:20:04 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,27 +2,50 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        a :name of "Lord Montague" and
+        an :age of "53".
         },
       :matriarch => {
-        #your key/value pairs here
+        a :name of "Lady Montague" and
+        an :age of "54".
         },
       :hero => {
-        #your key/value pairs here
+        a :name of "Romeo",
+        an :age of "15", and
+        a :status of "alive".
         },
-      :hero_friends => []
+      :hero_friends => [
+        {name: "Benvolio",
+   age: "17", attitude:
+   "worried"},
+       {name: "Mercutio",
+   age: "18", attitude:
+  "hot-headed"}
+  
+        ]
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        a :name of "Lord Capulet" and 
+        an :age of "50".
         },
       :matriarch => {
-        #your key/value pairs here
+        a :name of "Lady Capulet" and
+        an :age of "51", and
+        a :status of "alive".
         },
       :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
+        a :name of "Juliet",
+        an :age of "15", and 
+        a :status of "alive".},
+      :heroine_friends => [
+          {name: "Steven", 
+      age: "30", attitude:
+      "confused"}
+                {name: "Nurse", 
+      age: "44", attitude:
+      "worried"}
+      ]
    }
   }
 
